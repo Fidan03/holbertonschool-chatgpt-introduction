62ffecc920878ff02ca2464d3310850a3d8ea051
fkhalilova <fkhalilova@azerpost.az>
2026-03-31 10:29:12 +0400

    first commit

---
+++ debugging/factorial.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: ./factorial.py <number>")
+        exit(1)
+
+    try:
+        num = int(sys.argv[1])
+        if num < 0:
+            print("Factorial is not defined for negative numbers")
+            exit(1)
+
+        f = factorial(num)
+        print(f)
+
+    except ValueError:
+        print("Please provide a valid integer")
+        exit(1)
